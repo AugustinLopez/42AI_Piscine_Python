cd6c883423bed796d9739b8839435540d9616e18
Augustin LOPEZ <aulopez@e1r7p6.42.fr>
2021-12-02 18:09:43 +0100

    graph on kmean

--- M3/ex04/Kmeans.py
+++ M3/ex04/Kmeans.py
@@ -3,10 +3,15 @@
 from sys import argv
 from typing import Tuple, Optional
 import numpy as np
+from mpl_toolkits import mplot3d
+import matplotlib.pyplot as plt
+from matplotlib import rcParams
+
 
 class KmeansClustering:
-    def __init__(self, max_iter=20, ncentroid=4):
+    def __init__(self, max_iter=20, ncentroid=4, graph=False):
         # ... parameters control...
+        self.graph=graph
         self.n_cluster = ncentroid # number of clusters
         self.max_iter = max_iter # number of max iterations to update the centroids
         self.centroids = [] # values of the centroids
@@ -24,6 +29,21 @@ class KmeansClustering:
             centroids[k,:] = np.mean(X[closest == k, :], axis = 0)
         return centroids
 
+    def __graph_init(self, X):
+        self.fig = plt.figure()
+        self.ax = plt.axes(projection='3d')
+        self.ax.set_title('3d Scatter plot')
+        self.YC=np.linspace(0, 255, num=self.n_cluster, endpoint=True)
+
+    def __graph_iterate(self, X):
+        plt.cla()
+        Y = self.centroids
+        lst = np.argmin(self.__compute_distance(X, self.centroids), axis=1)
+        XC=[self.YC[i] for i in lst]
+        self.ax.scatter(X[..., 1], X[..., 2], X[..., 0], c= XC, alpha=0.3)
+        self.ax.scatter(Y[..., 1], Y[..., 2], Y[..., 0], c= self.YC, alpha=1.0, s=(rcParams['lines.markersize']*1.5) ** 2, marker='X')
+        self.fig.canvas.draw()
+        plt.pause(0.2)
 
     def fit(self, X):
         """
@@ -36,13 +56,18 @@ class KmeansClustering:
         Raises:
         This function should not raise any Exception.
         """
+        if self.graph:
+            self.__graph_init(X)
         self.centroids = X[np.random.permutation(X.shape[0])[:self.n_cluster]]
         for i in range(self.max_iter):
             old_centroids = self.centroids
+            if self.graph:
+                self.__graph_iterate(X)
             distance = self.__compute_distance(X, old_centroids)
             self.centroids = self.__compute_centroids(X, distance)
             if np.all(old_centroids == self.centroids):
                 break
+        
 
     def predict(self, X):
         """
@@ -56,8 +81,8 @@ class KmeansClustering:
         """
         return np.argmin(self.__compute_distance(X, self.centroids), axis=1)
 
-def __parse_arg() -> Optional[Tuple[str, int, int]]:
-    if len(argv) > 4:
+def __parse_arg() -> Optional[Tuple[str, int, int, bool]]:
+    if len(argv) > 5:
         print("too many argument")
         return None
     elif len(argv) < 1:
@@ -72,6 +97,7 @@ def __parse_arg() -> Optional[Tuple[str, int, int]]:
     filepath: str = None
     ncentroid: int = 4
     max_iter: int = 20
+    graph: bool = False
     for elem in lst:
         if elem[0] == 'filepath':
             filepath = elem[1]
@@ -87,28 +113,85 @@ def __parse_arg() -> Optional[Tuple[str, int, int]]:
             except ValueError:
                 print("cannot convert to int: ", elem[1])
                 return None
+        elif elem[0] == 'graph':
+            if elem[1] == 'true':
+                graph = True
         else:
             print("unknown argument: " + elem[0])
             return None
-    return filepath, ncentroid, max_iter
+    return filepath, ncentroid, max_iter, graph
 
-def __my_main() -> int:
-    arg = __parse_arg()
-    if arg is None:
-        return -1
-    X = np.array([str(x).rstrip().split(',')[1:] for x in open(arg[0])][1:], dtype=float)
-    kmean = KmeansClustering(arg[2], arg[1])
+def __fit_loop(arg, X):
+    kmean = KmeansClustering(arg[2], arg[1], arg[3])
     kmean.fit(X)
     lst = kmean.predict(X)
-    if (arg[1] == 4):
-        res = []
+    if arg[1] != 4:
+        return lst
+    while True:
         h_rank = []
         w_rank = []
         d_rank = []
         for i in range(4):
             h_rank.append([i, np.mean(X[..., 0][lst == i])])
             w_rank.append([i, np.mean(X[..., 1][lst == i])])
-            d_rank.append([i, np.mean(X[..., 1][lst == i])])
+            d_rank.append([i, np.mean(X[..., 2][lst == i])])
+        h_rank = np.array(h_rank)
+        w_rank = np.array(w_rank)
+        d_rank = np.array(d_rank)
+        h_rank = h_rank[h_rank[:,1].argsort()]
+        d_rank = d_rank[d_rank[:,1].argsort()[::-1]]
+        if h_rank[3][0] != d_rank[3][0] and h_rank[2][0] != d_rank[2][0]:
+            print("Bad run")
+            kmean.fit(X)
+            lst = kmean.predict(X)
+        else:
+            print("Good run")
+            d_rank = d_rank[d_rank[:,0].argsort()]
+            i = int(h_rank[3][0])
+            print("\nAsteroids' Belt colonies  : {:3}".format(np.count_nonzero(lst == i)))
+            print("H:{:7.2f}, W:{:6.2f}, B:{:5.2f}".format(h_rank[3][1], w_rank[i][1], d_rank[i][1]))
+            i = int(h_rank[2][0])
+            print("\nMars Republic             : {:3}".format(np.count_nonzero(lst == i)))
+            print("H:{:7.2f}, W:{:6.2f}, B:{:5.2f}".format(h_rank[2][1], w_rank[i][1], d_rank[i][1]))
+            i = max(int(h_rank[3][0]), int(h_rank[2][0]))
+            j = min(int(h_rank[3][0]), int(h_rank[2][0]))
+            w_rank = np.delete(w_rank, i, axis = 0)
+            w_rank = np.delete(w_rank, j, axis = 0)
+            w_rank = w_rank[w_rank[:,1].argsort()]
+            h_rank = h_rank[h_rank[:,0].argsort()]
+            i = int(w_rank[0][0])
+            print("\nThe flying cities of Venus: {:3}".format(np.count_nonzero(lst == w_rank[0][0])))
+            print("H:{:7.2f}, W:{:6.2f}, B:{:5.2f}".format(h_rank[i][1], w_rank[0][1], d_rank[i][1]))
+            i = int(w_rank[1][0])
+            print("\nUnited Nations of Earth   : {:3}".format(np.count_nonzero(lst == w_rank[1][0])))
+            print("H:{:7.2f}, W:{:6.2f}, B:{:5.2f}".format(h_rank[i][1], w_rank[1][1], d_rank[i][1]))
+            if kmean.graph:
+                plt.show()
+            break
+    return lst
+
+
+
+def __my_main() -> int:
+    arg = __parse_arg()
+    if arg is None:
+        return -1
+    X = np.array([str(x).rstrip().split(',')[1:] for x in open(arg[0])][1:], dtype=float)
+    lst = __fit_loop(arg, X)
+    for i in range(arg[1]):
+        print("{}: {}".format(i, np.count_nonzero(lst == i)))
+    return 0
+    kmean = KmeansClustering(arg[2], arg[1], arg[3])
+    kmean.fit(X)
+    lst = kmean.predict(X)
+    if (arg[1] == 4):
+        h_rank = []
+        w_rank = []
+        d_rank = []
+        for i in range(4):
+            h_rank.append([i, np.mean(X[..., 0][lst == i])])
+            w_rank.append([i, np.mean(X[..., 1][lst == i])])
+            d_rank.append([i, np.mean(X[..., 2][lst == i])])
             print("{}: {}".format(i, np.count_nonzero(lst == i)))
         h_rank = np.array(h_rank)
         w_rank = np.array(w_rank)
@@ -116,26 +199,17 @@ def __my_main() -> int:
         h_rank = h_rank[h_rank[:,1].argsort()]
         d_rank = d_rank[d_rank[:,1].argsort()[::-1]]
         if h_rank[3][0] == d_rank[3][0]:
-            print("Good run")
+            i = max(int(h_rank[3][0]), int(h_rank[2][0]))
+            j = min(int(h_rank[3][0]), int(h_rank[2][0]))
+            w_rank = np.delete(w_rank, i, axis = 0)
+            w_rank = np.delete(w_rank, j, axis = 0)
+            w_rank = w_rank[w_rank[:,1].argsort()[::-1]]
+            print("Asteroids' Belt colonies  : {}".format(np.count_nonzero(lst == h_rank[3][0])))
+            print("Mars Republic             : {}".format(np.count_nonzero(lst == h_rank[2][0])))
+            print("The flying cities of Venus: {}".format(np.count_nonzero(lst == w_rank[0][0])))
+            print("United Nations of Earth   : {}".format(np.count_nonzero(lst == w_rank[1][0])))
         else:
             print("Bad run")
-        print("Asteroids' Belt colonies: {}".format(np.count_nonzero(lst == h_rank[3][0])))
-        w_rank = np.array(w_rank)
-        w_rank = np.delete(w_rank, int(h_rank[3][0]), axis = 0)
-        w_rank = w_rank[w_rank[:,1].argsort()]
-        h_rank = np.delete(h_rank, 3, axis = 0)
-        venus = (w_rank[0][0],w_rank[1][0])
-        martian = (h_rank[2][0], h_rank[1][0])
-        earth1 = (w_rank[2][0], w_rank[1][0])
-        earth2 = (h_rank[0][0], h_rank[1][0])
-        print(venus)
-        print(martian)
-        print(earth1)
-        print(earth2)
-
-
-
-
 
 
 if __name__== "__main__":
